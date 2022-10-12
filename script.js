3774dbef1bade962b9883e7d330f083b0972add3
Lettie02 <nahmanyesther1@gmail.com>
2022-10-12 14:26:25 +0300

    trial

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function gitGraph() {
-  console.log("hi");
-}
+// function gitGraph() {
+//   console.log("hi");
+// }
+
+// const array = [-1, 0, 3, 100, 99, 2, 99];
+// const array2 = ["a", 3, 4, 2];
+// const array3 = [];
+// function biggestNumberInArray(arrayNumber) {
+//   arrayNumber.forEach((num) => {
+//     num > max ? (max = num) : null;
+//   });
+// }
+
+prompt.user("Enter words seperated with commas");
